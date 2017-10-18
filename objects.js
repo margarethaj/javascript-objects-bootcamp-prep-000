23d545a253c791f2c986216f994c3d5d0b968fd1
Margaretha Juan <tulip_holy@yahoo.com>
2017-10-18 01:18:12 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = new Object({ namaArtist : 'judulLagu'});
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign({}, playlist, {artistName : songTitle} )
+  return Object.assign({}, playlist, {[artistName] : songTitle} )
 }
